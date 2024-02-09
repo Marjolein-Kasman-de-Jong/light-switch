9fe422a9ad7791f111587807337a9f3352779ba0
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-02-09 15:54:06 +0100

    Finished assignment

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+    const [light, toggleLight] = React.useState(false)
+    console.log(light)
+
+
     return (
-        <main className="off">
+        <main className={light ? "on" : "off"}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button 
+                    type="button"
+                    onClick={light ? () => toggleLight(false) : () => toggleLight(true)}
+                >
+                    {light? "Turn off" : "Turn on"}
+                </button>
             </section>
         </main>
     );
